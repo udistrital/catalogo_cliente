86fd40861925a2e1b21401e68b6419732032637a
fredy <megablastoise94@gmail.com>
2019-09-16 11:20:37 -0500

    Maquetacion base para los aplicativos por medio de archivo json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import data from './data.json';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'interfaz-unica-cliente';
+  data:any = data;
 }
