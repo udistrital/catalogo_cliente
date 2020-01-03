df881eeab5d3bd9d65393bfba55607effc90573e
Fabián Sánchez L <fadarsaleeing@gmail.com>
2020-01-03 12:59:22 -0500

    Resolviendo claims, ajuste de lib para evitar error no_role

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,7 +13,7 @@ export const environment = {
     AUTORIZATION_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize',
     CLIENTE_ID: 'e36v1MPQk2jbz9KM4SmKhk8Cyw0a',
     RESPONSE_TYPE: 'id_token token',
-    SCOPE: 'openid email role documento',
+    SCOPE: 'openid email',
     REDIRECT_URL: 'http://localhost:4200/',
     SIGN_OUT_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oidc/logout',
     SIGN_OUT_REDIRECT_URL: 'http://localhost:4200/',
