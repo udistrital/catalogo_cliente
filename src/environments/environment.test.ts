e03a89658182750d49079670bd16992dae43098f
Fabián Sánchez L <fadarsaleeing@gmail.com>
2020-01-21 13:05:40 -0500

    solve service test

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -10,16 +10,17 @@ export const environment = {
   menuApps: true,
   CONFIGURACION_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/configuracion_crud_api/v1/',
   NOTIFICACION_SERVICE: 'wss://pruebasapi.portaloas.udistrital.edu.co:8116/ws',
-    TOKEN: {
-        AUTORIZATION_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize',
-      CLIENTE_ID: 'yvXIb7DEJXCCvTvpkBTyQJthBj8a',
-      RESPONSE_TYPE: 'id_token token',
-      SCOPE: 'openid email role documento',
-      REDIRECT_URL: 'https://pruebascatalogo.portaloas.udistrital.edu.co',
-      SIGN_OUT_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oidc/logout',
-      SIGN_OUT_REDIRECT_URL: 'https://pruebascatalogo.portaloas.udistrital.edu.co',
-    },
-  };
+  TOKEN: {
+    AUTORIZATION_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize',
+    CLIENTE_ID: 'yvXIb7DEJXCCvTvpkBTyQJthBj8a',
+    RESPONSE_TYPE: 'id_token token',
+    SCOPE: 'openid email',
+    REDIRECT_URL: 'https://pruebascatalogo.portaloas.udistrital.edu.co',
+    SIGN_OUT_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oidc/logout',
+    SIGN_OUT_REDIRECT_URL: 'https://pruebascatalogo.portaloas.udistrital.edu.co',
+    AUTENTICACION_MID: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/autenticacion_mid/v1/token/userRol',
+  },
+};
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
